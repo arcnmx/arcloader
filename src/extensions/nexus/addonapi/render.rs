d336cf28446154e453050aae3146705514610f5d
arcnmx <git@git.arcn.mx>
2025-05-02 23:33:54 -0700

    chore(addonapi/render): context accessors

--- src/extensions/nexus/addonapi/render.rs
+++ src/extensions/nexus/addonapi/render.rs
@@ -1,7 +1,28 @@
+use arcdps::dxgi_swap_chain;
 use nexus::gui::RawGuiRender;
+use windows::core::InterfaceRef;
+use windows::Win32::Foundation::ERROR_API_UNAVAILABLE;
+use windows::Win32::Graphics::Direct3D11::{ID3D11Device, ID3D11DeviceContext};
+use windows::Win32::Graphics::Dxgi::IDXGISwapChain;
 use crate::extensions::nexus::NexusHost;
+use crate::util::win::{WinResult, WinError};
 
 impl NexusHost {
+	pub fn dxgi_swap_chain() -> WinResult<InterfaceRef<'static, IDXGISwapChain>> {
+		dxgi_swap_chain()
+			.ok_or_else(|| WinError::new(ERROR_API_UNAVAILABLE.to_hresult(), "D3D11 context not available"))
+	}
+
+	pub fn dxgi_device() -> WinResult<ID3D11Device> {
+		Self::dxgi_swap_chain()
+			.and_then(|sc| unsafe { sc.GetDevice() })
+	}
+
+	pub fn dxgi_device_context() -> WinResult<ID3D11DeviceContext> {
+		Self::dxgi_device()
+			.and_then(|dev| unsafe { dev.GetImmediateContext() })
+	}
+
 	pub unsafe extern "C-unwind" fn addonapi_renderer_register(render_type: nexus::gui::RenderType, render_callback: RawGuiRender) {
 		addonapi_stub!(renderer::add("{:?}, {:?}", render_type, render_callback));
 
