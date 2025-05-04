a3045d3f209c75bf0a7d5de790bb017cd8894173
arcnmx <git@git.arcn.mx>
2025-05-03 17:58:37 -0700

    chore: bump nexus

--- lock.nix
+++ lock.nix
@@ -4,6 +4,6 @@
     "arcdps_codegen-0.11.0" = "sha256-8Mxm7O+HejUHvHGWFcgejEEACfDPZwIm+8sBUTL8zDo=";
     "evtc-0.9.2" = "sha256-8Mxm7O+HejUHvHGWFcgejEEACfDPZwIm+8sBUTL8zDo=";
     "gw2_mumble-0.2.5" = "sha256-DvZDSqh66NqzoLL4cKfPJdSPBnTFqA3xOmcTpgoICGE=";
-    "nexus-0.7.0" = "sha256-1ZQM/dsAlOMW17jRF3EJj1Zx0xIGcK3vBrasBITx6N0=";
+    "nexus-0.8.0" = "sha256-AgiLWmnlP9YP/AFuPccY6hyF88El3KB+UqgkTrgqUmY=";
   };
 }
